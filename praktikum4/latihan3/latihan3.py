f9234f44e1b25a736973f97ce9aba246fdb41d03
Annisa Rachmawati <71623132+annisarac@users.noreply.github.com>
2020-10-25 14:18:25 +0700

    Create latihan3.py

---
+++ praktikum4/latihan3/latihan3.py
@@ -0,0 +1,8 @@
+sum = 0
+jumlah = 0
+for i in range(1, 100, 2):
+    print(i)
+    sum = sum + 1
+    jumlah = jumlah + i
+print('Banyaknya bilangan ganjil :' +str(sum))
+print('Jumlah seluruh bilangan :' +str(jumlah))
